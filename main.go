f54992b81bc5f56b9290b4074c034e8090d1ce3d
Skorgum <skorgum@gmail.com>
2025-12-14 20:21:49 -0500

    Add agg command

--- main.go
+++ main.go
@@ -35,18 +35,19 @@ func main() {
 		cfg: &cfg,
 	}
 
-	commands := &commands{registeredCommands: make(map[string]func(*state, command) error)}
-	commands.register("login", handlerLogin)
-	commands.register("register", handlerRegister)
-	commands.register("reset", handlerReset)
-	commands.register("users", handlerListUsers)
+	cmds := &commands{registeredCommands: make(map[string]func(*state, command) error)}
+	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerListUsers)
+	cmds.register("agg", handlerAgg)
 
 	if len(os.Args) < 2 {
 		fmt.Println("No command provided")
 		os.Exit(1)
 	}
 	cmd := command{os.Args[1], os.Args[2:]}
-	err = commands.run(s, cmd)
+	err = cmds.run(s, cmd)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
